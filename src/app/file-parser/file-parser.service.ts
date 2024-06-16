a6bcc5c951b00a0ef8c0e49b09d599cfa5d42181
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-16 15:10:56 +0200

    inject TransactionService to save transaction entities to db

--- src/app/file-parser/file-parser.service.ts
+++ src/app/file-parser/file-parser.service.ts
@@ -4,6 +4,7 @@ import { FileUpload } from 'graphql-upload';
 import { BankTransactionAdapterFactory } from 'src/app/bank-transaction-adapter/bank-transaction-adapter.factory';
 import { Bank } from 'src/app/bank-transaction-adapter/constants/bank';
 import { ReadStream } from 'fs-capacitor';
+import { TransactionService } from '../transaction/transaction.service';
 import { Transaction } from '../transaction/entities/transaction.entity';
 
 @Injectable()
@@ -11,6 +12,7 @@ export class FileParserService {
   constructor(
     private readonly fileParserFactory: FileParserFactory,
     private readonly bankTransactionAdapterFactory: BankTransactionAdapterFactory,
+    private readonly transactionService: TransactionService,
   ) {}
 
   async parse(file: FileUpload, bank: Bank): Promise<Transaction[]> {
@@ -18,12 +20,15 @@ export class FileParserService {
     const stream = createReadStream();
 
     const buffer = await this.readStream(stream);
-
     const fileParser = this.fileParserFactory.create(mimetype);
     const data = fileParser.parse(buffer);
 
     const adapter = this.bankTransactionAdapterFactory.create(bank);
-    const transactions = adapter.map(data);
+    const createTransactionDtos = await adapter.map(data);
+
+    const transactions = await this.transactionService.create(
+      createTransactionDtos,
+    );
 
     return transactions;
   }
