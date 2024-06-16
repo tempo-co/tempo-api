f831d50cdba3bb302fbd91afcf3383a9749bdfb1
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-16 02:32:54 +0200

    disable implicit conversion and add additional validation decorators

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -2,8 +2,10 @@ import { plainToInstance } from 'class-transformer';
 import {
   IsBoolean,
   IsEnum,
+  IsNotEmpty,
   IsNumber,
   IsString,
+  Matches,
   Max,
   Min,
   validateSync,
@@ -24,9 +26,13 @@ class EnvironmentVariables {
   PORT: number;
 
   @IsString()
+  @IsNotEmpty()
+  @Matches(/\S/, { message: 'DB_TYPE cannot be whitespace' })
   DB_TYPE: string;
 
   @IsString()
+  @IsNotEmpty()
+  @Matches(/\S/, { message: 'DB_HOST cannot be whitespace' })
   DB_HOST: string;
 
   @IsNumber()
@@ -35,25 +41,31 @@ class EnvironmentVariables {
   DB_PORT: number;
 
   @IsString()
+  @IsNotEmpty()
+  @Matches(/\S/, { message: 'DB_USERNAME cannot be whitespace' })
   DB_USERNAME: string;
 
   @IsString()
+  @IsNotEmpty()
+  @Matches(/\S/, { message: 'DB_PASSWORD cannot be whitespace' })
   DB_PASSWORD: string;
 
   @IsString()
+  @IsNotEmpty()
+  @Matches(/\S/, { message: 'DB_NAME cannot be whitespace' })
   DB_NAME: string;
 
   @IsBoolean()
   DB_SYNCHRONIZE: boolean;
 
   @IsString()
+  @IsNotEmpty()
+  @Matches(/\S/, { message: 'JWT_SECRET cannot be whitespace' })
   JWT_SECRET: string;
 }
 
 export function validate(config: Record<string, unknown>) {
-  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
-    enableImplicitConversion: true,
-  });
+  const validatedConfig = plainToInstance(EnvironmentVariables, config);
   const errors = validateSync(validatedConfig, {
     skipMissingProperties: false,
   });
