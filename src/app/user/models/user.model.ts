969ea23061e2370dcad617e006c2ede616b5bebf
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-16 15:13:58 +0200

    remove validation decorators from User ObjectType

--- src/app/user/models/user.model.ts
+++ src/app/user/models/user.model.ts
@@ -1,5 +1,4 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsDate, IsEmail, IsString, Length } from 'class-validator';
 
 @ObjectType()
 export class User {
@@ -7,20 +6,14 @@ export class User {
   id: string;
 
   @Field()
-  @IsString()
-  @Length(1, 255)
   name: string;
 
   @Field()
-  @IsEmail()
-  @Length(1, 255)
   email: string;
 
   @Field()
-  @IsBoolean()
   isActive: boolean;
 
   @Field()
-  @IsDate()
   createdDate: Date;
 }
