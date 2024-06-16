fc8416162268a4fcbc3cf12c7e78b036e7cf8e65
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-16 14:49:15 +0200

    remove validation decorators from Transaction ObjectType

--- src/app/transaction/models/transaction.model.ts
+++ src/app/transaction/models/transaction.model.ts
@@ -1,5 +1,4 @@
 import { Field, Float, ID, ObjectType } from '@nestjs/graphql';
-import { IsDate, IsNumber, IsString, Length, Max, Min } from 'class-validator';
 
 @ObjectType()
 export class Transaction {
@@ -7,26 +6,17 @@ export class Transaction {
   id: string;
 
   @Field()
-  @IsDate()
   startedDate: Date;
 
   @Field()
-  @IsDate()
   completedDate: Date;
 
   @Field()
-  @IsString()
-  @Length(1, 255)
   description: string;
 
   @Field(() => Float)
-  @IsNumber({ maxDecimalPlaces: 2 })
-  @Min(0)
-  @Max(999999999999.99)
   amount: number;
 
   @Field()
-  @IsString()
-  @Length(3, 3)
   currency: string;
 }
