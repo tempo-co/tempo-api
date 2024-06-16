bac9089a74a2a47eb3797308269c26094262ab7c
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-16 15:09:44 +0200

    add create repository method

--- src/app/transaction/transaction.service.ts
+++ src/app/transaction/transaction.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Transaction } from './entities/transaction.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
 
 @Injectable()
 export class TransactionService {
@@ -16,5 +17,9 @@ export class TransactionService {
 
   findById(id: string): Promise<Transaction | null> {
     return this.transactionRepository.findOne({ where: { id } });
+  }
+
+  create(transactions: CreateTransactionDto[]): Promise<Transaction[]> {
+    return this.transactionRepository.save(transactions);
   }
 }
