84e8ff47e88143a18ae5215028badf6aaabce95f
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-17 11:51:57 +0200

    register Bank enum type in gql schema

--- src/app/bank-transaction-adapter/constants/bank.ts
+++ src/app/bank-transaction-adapter/constants/bank.ts
@@ -1,6 +1,11 @@
-// Supported banks
+import { registerEnumType } from '@nestjs/graphql';
 
 export enum Bank {
   ABN_AMRO = 'ABN_AMRO',
   REVOLUT = 'REVOLUT',
 }
+
+registerEnumType(Bank, {
+  name: 'Bank',
+  description: 'Supported banks',
+});
