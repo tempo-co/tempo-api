f3c19fdd8a017e7739bdcc8cdbff7077d521a031
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-18 19:58:08 +0200

    specify graphql types for args

--- src/app/api/graphql/modules/bank-statements/graphql/bank-statement.mutations.resolver.ts
+++ src/app/api/graphql/modules/bank-statements/graphql/bank-statement.mutations.resolver.ts
@@ -2,7 +2,7 @@ import {Resolver, Args, Mutation} from '@nestjs/graphql';
 import {TypeBankStatement} from './bank-statement.type';
 import {Transaction} from 'src/app/entities/transaction/transaction.entity';
 import {StatementService} from '../services/statement.service';
-import {FileUpload} from 'graphql-upload';
+import {FileUpload, GraphQLUpload} from 'graphql-upload';
 import {Account} from 'src/app/entities/account/account.entity';
 
 @Resolver(() => TypeBankStatement)
@@ -11,8 +11,8 @@ export class StatementResolver {
 
   @Mutation(() => [Transaction])
   async uploadStatement(
-    @Args('file') file: FileUpload,
-    @Args('accountId') accountId: Account['id'],
+    @Args('file', {type: () => GraphQLUpload}) file: FileUpload,
+    @Args('accountId', {type: () => String}) accountId: Account['id'],
   ): Promise<Transaction[]> {
     const transactions = await this.statementService.processStatement({file, accountId});
     return transactions;
