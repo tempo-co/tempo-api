9cd83551b7a4a47aa4bb8fe70ae2db32df9bc93a
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-19 19:35:20 +0200

    refactor method to use ArgsType

--- src/app/api/graphql/modules/accounts/graphql/account.mutations.resolver.ts
+++ src/app/api/graphql/modules/accounts/graphql/account.mutations.resolver.ts
@@ -3,17 +3,14 @@ import {User} from '@entities/user/user.entity';
 import {CurrentUser} from '@core/auth/decorators/current-user.decorator';
 import {AccountService} from '../services/account.service';
 import {TypeAccount} from './account.type';
-import {InputAccountCreate} from './account-create.input';
+import {ArgsAccountCreate} from './account-create.args';
 
 @Resolver(() => TypeAccount)
 export class AccountResolver {
   constructor(private readonly accountService: AccountService) {}
 
   @Mutation(() => TypeAccount)
-  accountCreate(
-    @Args('inputAccountCreate') inputAccountCreate: InputAccountCreate,
-    @CurrentUser() user: User,
-  ): Promise<TypeAccount> {
-    return this.accountService.create(inputAccountCreate, user.id);
+  accountCreate(@Args() args: ArgsAccountCreate, @CurrentUser() user: User): Promise<TypeAccount> {
+    return this.accountService.create({...args, userId: user.id});
   }
 }
