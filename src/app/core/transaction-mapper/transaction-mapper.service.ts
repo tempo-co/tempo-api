6466ef7c67728025798b86adf56779aaa97c205d
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-19 13:11:27 +0200

    handle iterating over the data and validating transactions in service

--- src/app/core/transaction-mapper/transaction-mapper.service.ts
+++ src/app/core/transaction-mapper/transaction-mapper.service.ts
@@ -1,7 +1,8 @@
-import {Injectable} from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {InputTransactionCreate} from '@modules/transactions/graphql/transaction-create.input';
 import {TransactionMapperFactory} from './transaction-mapper.factory';
 import {Bank} from './models/bank.enum';
+import {validate} from 'class-validator';
 
 @Injectable()
 export class TransactionMapperService {
@@ -9,7 +10,21 @@ export class TransactionMapperService {
 
   async map(data: unknown[], bank: Bank): Promise<InputTransactionCreate[]> {
     const mapper = this.transactionMapperFactory.create(bank);
-    const transactions = await mapper.map(data);
+    // TODO: Don't use InputTransactionCreate
+    const transactions = await Promise.all(
+      data.map(async (rawTransaction) => {
+        const transaction = mapper.map(rawTransaction);
+
+        const validationErrors = await validate(transaction);
+
+        if (validationErrors.length > 0) {
+          throw new BadRequestException(
+            'Validation failed for a transaction: ' + validationErrors.toString(),
+          );
+        }
+        return transaction;
+      }),
+    );
     return transactions;
   }
 }
