22e7f38af75d08e4829a9024b07ce7e66b3dd2b4
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-19 19:34:07 +0200

    use ['id'] instead of string and avoid passing InputType as method param

--- src/app/api/graphql/modules/accounts/services/account.service.ts
+++ src/app/api/graphql/modules/accounts/services/account.service.ts
@@ -3,7 +3,14 @@ import {Injectable, NotFoundException} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Account} from '@entities/account/account.entity';
 import {UserService} from '@modules/users/services/user.service';
-import {InputAccountCreate} from '../graphql/account-create.input';
+import {Bank} from '@core/transaction-mapper/models/bank.enum';
+import {User} from '@entities/user/user.entity';
+
+type CreateOptions = {
+  alias: Account['alias'];
+  bank: Bank;
+  userId: User['id'];
+};
 
 @Injectable()
 export class AccountService {
@@ -13,18 +20,18 @@ export class AccountService {
     private readonly userService: UserService,
   ) {}
 
-  async create(inputAccountCreate: InputAccountCreate, userId: string): Promise<Account> {
+  async create({alias, bank, userId}: CreateOptions): Promise<Account> {
     const user = await this.userService.findById(userId);
 
-    const account = this.accountRepository.create({...inputAccountCreate, user});
+    const account = this.accountRepository.create({alias, bank, user});
     return this.accountRepository.save(account);
   }
 
-  async findAllByUserId(userId: string): Promise<Account[]> {
+  async findAllByUserId(userId: User['id']): Promise<Account[]> {
     return this.accountRepository.find({where: {user: {id: userId}}});
   }
 
-  async findById(id: string): Promise<Account> {
+  async findById(id: Account['id']): Promise<Account> {
     const account = await this.accountRepository.findOne({where: {id}});
 
     if (!account) {
