6fe742d8429c0ec847661f86a6ab5f8db44c2746
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-27 13:15:14 +0200

    add swagger, remove graphql config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import {ValidationPipe} from '@nestjs/common';
 import {ConfigService} from '@nestjs/config';
 import {NestFactory} from '@nestjs/core';
-import {altairExpress} from 'altair-express-middleware';
-import {graphqlUploadExpress} from 'graphql-upload';
+import {DocumentBuilder, SwaggerDocumentOptions, SwaggerModule} from '@nestjs/swagger';
 import helmet from 'helmet';
 
 import {AppModule} from './app.module';
@@ -10,27 +9,16 @@ import {AppModule} from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(
-    helmet({
-      crossOriginEmbedderPolicy: false,
-      // Config to solve issues with CSP when using Apollo Sandbox
-      contentSecurityPolicy: {
-        directives: {
-          imgSrc: [`'self'`, 'data:', 'apollo-server-landing-page.cdn.apollographql.com'],
-          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
-          manifestSrc: [`'self'`, 'apollo-server-landing-page.cdn.apollographql.com'],
-          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
-        },
-      },
-    }),
-  );
-
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
-  app.use(graphqlUploadExpress({maxFileSize: 100000000, maxFiles: 2}));
 
-  // Uses Altair client to test file upload since Apollo Sandbox
-  // sends all files as application/octet-stream MIME type
-  app.use('/altair', altairExpress({endpointURL: '/graphql'}));
+  const config = new DocumentBuilder().setTitle('Flair API').build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api', app, document);
 
   const port = app.get(ConfigService).get('PORT');
   await app.listen(port);
