1b4644310a94e1be8b52adb655e249f26277811d
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-29 21:12:07 +0200

    fix CurrentUser decorator to correctly extract user from request context

--- src/app/core/auth/decorators/current-user.decorator.ts
+++ src/app/core/auth/decorators/current-user.decorator.ts
@@ -1,12 +1,9 @@
-import {UnauthorizedException, createParamDecorator} from '@nestjs/common';
-import {Request} from 'express';
+import {ExecutionContext, createParamDecorator} from '@nestjs/common';
 
 /**
  * Extracts the current user from the request context.
  */
-export const CurrentUser = createParamDecorator((_data, req: Request) => {
-  if (!req.user) {
-    throw new UnauthorizedException();
-  }
-  return req.user;
+export const CurrentUser = createParamDecorator((_data, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  return request.user;
 });
