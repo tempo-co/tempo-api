ab291e2c46c5c6ff4cfdb055decd8a6fa5ff4f34
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-29 21:19:39 +0200

    enable cors with default settings

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
