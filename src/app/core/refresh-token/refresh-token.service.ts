76d7750ad5ac97855c5c1f488c6410f5778f1235
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-03 00:18:51 +0200

    get expiration from config and use ms package to calculate expiryDate

--- src/app/core/refresh-token/refresh-token.service.ts
+++ src/app/core/refresh-token/refresh-token.service.ts
@@ -1,5 +1,7 @@
 import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
 import * as argon2 from 'argon2';
+import ms from 'ms';
 
 import {RefreshToken} from '@entities/refresh-token/refresh-token.entity';
 import {RefreshTokenRepository} from '@entities/refresh-token/refresh-token.repository';
@@ -8,10 +10,16 @@ import {UserService} from '@modules/users/user.service';
 
 @Injectable()
 export class RefreshTokenService {
+  private readonly REFRESH_TOKEN_EXPIRATION: string;
+
   constructor(
     private readonly refreshTokenRepository: RefreshTokenRepository,
     private readonly userService: UserService,
-  ) {}
+    private readonly configService: ConfigService,
+  ) {
+    this.REFRESH_TOKEN_EXPIRATION =
+      this.configService.get<string>('REFRESH_TOKEN_EXPIRATION') || '14d';
+  }
 
   async save(userId: User['id']): Promise<RefreshToken> {
     const user = await this.userService.findById(userId).catch(() => {
@@ -21,7 +29,7 @@ export class RefreshTokenService {
     const refreshToken = crypto.randomUUID();
     const hash = await argon2.hash(refreshToken);
 
-    const expiryDate = new Date(); // TODO: set expiry date from ENV config
+    const expiryDate = new Date(Date.now() + ms(this.REFRESH_TOKEN_EXPIRATION));
 
     return this.refreshTokenRepository.save({user, token: hash, expiresAt: expiryDate});
   }
