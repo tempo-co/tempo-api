6d43923e43b908c1326a157138905ef04e419bf7
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-02 21:16:14 +0200

    replace class-validator for .env validation with joi validationSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import {ThrottlerGuard, ThrottlerModule, minutes} from '@nestjs/throttler';
 import {TypeOrmModule} from '@nestjs/typeorm';
 
 import {AuthModule} from '@core/auth/auth.module';
-import {validate} from '@core/config/env.validation';
+import {validationSchema} from '@core/config/validation-schema';
 import {FileParserModule} from '@core/file-parser/file-parser.module';
 import {TransactionCategorizerModule} from '@core/transaction-categorizer/transaction-categorizer.module';
 import {BankStatementModule} from '@modules/bank-statements/bank-statement.module';
@@ -15,7 +15,7 @@ import {UserModule} from '@modules/users/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validate,
+      validationSchema,
       envFilePath: ['.env.development.local', '.env.development'],
       cache: true,
       validationOptions: {
---
+++ src/app/core/config/validation-schema.ts
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production').required(),
+  PORT: Joi.number().min(0).max(65535).required(),
+  DB_TYPE: Joi.string().regex(/\S/).required(),
+  DB_HOST: Joi.string().regex(/\S/).required(),
+  DB_PORT: Joi.number().min(0).max(65535).required(),
+  DB_USERNAME: Joi.string().regex(/\S/).required(),
+  DB_PASSWORD: Joi.string().regex(/\S/).required(),
+  DB_NAME: Joi.string().regex(/\S/).required(),
+  DB_SYNCHRONIZE: Joi.boolean().required(),
+  ACCESS_TOKEN_SECRET: Joi.string().regex(/\S/).required(),
+  ACCESS_TOKEN_EXPIRATION: Joi.string().regex(/\S/).required(),
+  REFRESH_TOKEN_EXPIRATION: Joi.string().regex(/\S/).required(),
+  GEMINI_API_KEY: Joi.string().regex(/\S/).required(),
+});
