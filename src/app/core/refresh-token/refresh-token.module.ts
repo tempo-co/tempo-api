b4ab2510f24da256dc997b09d65402e06f48bc7c
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-03 00:23:41 +0200

    fix import

--- src/app/core/refresh-token/refresh-token.module.ts
+++ src/app/core/refresh-token/refresh-token.module.ts
@@ -1,4 +1,5 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 
 import {RefreshTokenRepositoryModule} from '@entities/refresh-token/refresh-token.repository.module';
 import {UserModule} from '@modules/users/user.module';
@@ -6,7 +7,7 @@ import {UserModule} from '@modules/users/user.module';
 import {RefreshTokenService} from './refresh-token.service';
 
 @Module({
-  imports: [RefreshTokenRepositoryModule, UserModule],
+  imports: [RefreshTokenRepositoryModule, UserModule, ConfigModule],
   providers: [RefreshTokenService],
   exports: [RefreshTokenService],
 })
