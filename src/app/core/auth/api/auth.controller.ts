b6a992444bc82cf530743c48da38401d848e6d86
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-02 20:47:40 +0200

    implement refresh tokens and store tokens in HttpOnly cookie

--- src/app/core/auth/api/auth.controller.ts
+++ src/app/core/auth/api/auth.controller.ts
@@ -1,5 +1,15 @@
-import {Body, Controller, HttpCode, Post, UseGuards} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import {ApiResponse} from '@nestjs/swagger';
+import {Request, Response} from 'express';
 
 import {User} from '@entities/user/user.entity';
 
@@ -7,9 +17,9 @@ import {CurrentUser} from '../decorators/current-user.decorator';
 import {Public} from '../decorators/public.decorator';
 import {LocalAuthGuard} from '../guards/local-auth.guard';
 import {AuthService} from '../services/auth.service';
-import {AccessTokenDto} from './access-token.dto';
 import {LogInDto} from './login.dto';
 import {SignUpDto} from './signup.dto';
+import {TokensDto} from './tokens.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -22,8 +32,28 @@ export class AuthController {
   @ApiResponse({status: 200, description: 'User has been successfully logged in.'})
   @ApiResponse({status: 400, description: 'Validation of the request body failed.'})
   @ApiResponse({status: 401, description: 'Invalid credentials.'})
-  logIn(@Body() _dto: LogInDto, @CurrentUser() user: User): Promise<AccessTokenDto> {
-    return this.authService.signAccessToken(user);
+  async logIn(
+    @Body() _dto: LogInDto,
+    @CurrentUser() user: User,
+    @Res({passthrough: true}) response: Response,
+  ): Promise<TokensDto> {
+    const tokens = await this.authService.generateTokens(user);
+
+    response.cookie('access_token', tokens.access_token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: false, // set to true in production
+      maxAge: 300000, // 5min
+    });
+
+    response.cookie('refresh_token', tokens.refresh_token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: false, // set to true in production
+      maxAge: 14 * 24 * 60 * 60 * 1000, // 14d
+    });
+
+    return tokens;
   }
 
   @Public()
@@ -31,7 +61,68 @@ export class AuthController {
   @ApiResponse({status: 201, description: 'The user has been successfully created.'})
   @ApiResponse({status: 400, description: 'Validation of the request body failed.'})
   @ApiResponse({status: 409, description: 'An account with this email already exists.'})
-  signUp(@Body() dto: SignUpDto): Promise<AccessTokenDto> {
-    return this.authService.createUser(dto);
+  async signUp(
+    @Body() dto: SignUpDto,
+    @Res({passthrough: true}) response: Response,
+  ): Promise<TokensDto> {
+    const tokens = await this.authService.createUser(dto);
+
+    response.cookie('access_token', tokens.access_token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: false, // set to true in production
+      maxAge: 300000, // 5min
+    });
+
+    response.cookie('refresh_token', tokens.refresh_token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: false, // set to true in production
+      maxAge: 14 * 24 * 60 * 60 * 1000, // 14d
+    });
+
+    return tokens;
+  }
+
+  @Post('logout')
+  @HttpCode(200)
+  @ApiResponse({status: 200, description: 'User has been successfully logged out.'})
+  @ApiResponse({status: 401, description: 'User is not logged in.'})
+  async logOut(@CurrentUser() user: User, @Res({passthrough: true}) response: Response) {
+    response.clearCookie('access_token');
+    response.clearCookie('refresh_token');
+    await this.authService.revokeRefreshToken(user.id);
+  }
+
+  @Post('refresh')
+  @HttpCode(200)
+  @ApiResponse({status: 200, description: 'Tokens have been successfully refreshed.'})
+  @ApiResponse({status: 401, description: 'Missing or invalid refresh token.'})
+  async refreshTokens(
+    @Req() request: Request,
+    @Res({passthrough: true}) response: Response,
+  ): Promise<TokensDto> {
+    const refreshToken = request.cookies?.refresh_token;
+
+    if (!refreshToken || refreshToken.length === 0) {
+      throw new UnauthorizedException();
+    }
+    const tokens = await this.authService.refreshTokens(refreshToken);
+
+    response.cookie('access_token', tokens.access_token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: false, // set to true in production
+      maxAge: 300000, // 5min
+    });
+
+    response.cookie('refresh_token', tokens.refresh_token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: false, // set to true in production
+      maxAge: 14 * 24 * 60 * 60 * 1000, // 14d
+    });
+
+    return tokens;
   }
 }
--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -2,45 +2,55 @@ import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {JwtService} from '@nestjs/jwt';
 import * as argon2 from 'argon2';
 
+import {RefreshTokenService} from '@core/refresh-token/refresh-token.service';
+import {RefreshToken} from '@entities/refresh-token/refresh-token.entity';
 import {User} from '@entities/user/user.entity';
 import {UserSaveOptions} from '@entities/user/user.repository';
 import {UserService} from '@modules/users/user.service';
 
-import {AccessTokenDto} from '../api/access-token.dto';
+import {TokensDto} from '../api/tokens.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly refreshTokenService: RefreshTokenService,
   ) {}
 
   async validateUser(email: string, password: string): Promise<User> {
-    let user: User;
-
-    try {
-      user = await this.userService.findByEmail(email);
-    } catch (error) {
+    const user = await this.userService.findByEmail(email).catch(() => {
       throw new UnauthorizedException();
-    }
+    });
 
-    const passwordMatches = await argon2.verify(user.password, password);
+    const isPasswordValid = await argon2.verify(user.password, password);
 
-    if (!passwordMatches) {
+    if (!isPasswordValid) {
       throw new UnauthorizedException();
     }
     return user;
   }
 
-  async createUser(options: UserSaveOptions): Promise<AccessTokenDto> {
+  async createUser(options: UserSaveOptions): Promise<TokensDto> {
     const user = await this.userService.save(options);
-    return await this.signAccessToken(user);
+    return await this.generateTokens(user);
   }
 
-  async signAccessToken(user: User): Promise<AccessTokenDto> {
-    const payload = {sub: user.id, email: user.email};
+  async generateTokens(user: User): Promise<TokensDto> {
+    const refreshToken = await this.refreshTokenService.save(user.id);
 
-    const jwt = await this.jwtService.signAsync(payload);
-    return {access_token: jwt};
+    const payload = {sub: user.id, email: user.email};
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return {access_token: accessToken, refresh_token: refreshToken.token};
+  }
+
+  async refreshTokens(refreshToken: RefreshToken['token']): Promise<TokensDto> {
+    const validRefreshToken = await this.refreshTokenService.findByToken(refreshToken);
+    return this.generateTokens(validRefreshToken.user);
+  }
+
+  async revokeRefreshToken(refreshToken: RefreshToken['token']) {
+    this.refreshTokenService.delete(refreshToken);
   }
 }
