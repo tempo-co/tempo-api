3761ba7186ef075e40e0fb85efe4730511a16871
eduard-cc <eduard.ciociu22@gmail.com>
2024-07-17 13:58:41 +0200

    add /accounts/:id route

--- src/app/api/modules/accounts/account.controller.ts
+++ src/app/api/modules/accounts/account.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, ParseUUIDPipe, Post} from '@nestjs/common';
 
 import {CurrentUser} from '@core/auth/decorators/current-user.decorator';
 import {Account} from '@entities/account/account.entity';
@@ -16,8 +16,16 @@ export class AccountController {
     return this.accountService.save(dto, user.id);
   }
 
-  @Get('me')
-  getCurrentUserAccounts(@CurrentUser() user: User): Promise<Account[]> {
+  @Get()
+  getAllAccounts(@CurrentUser() user: User): Promise<Account[]> {
     return this.accountService.findAllByUserId(user.id);
+  }
+
+  @Get(':id')
+  getAccountById(
+    @Param('id', new ParseUUIDPipe({version: '4'})) accountId: Account['id'],
+    @CurrentUser() user: User,
+  ): Promise<Account> {
+    return this.accountService.findById(accountId, user.id);
   }
 }
